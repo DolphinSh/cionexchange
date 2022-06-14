1a12e8fe1ae6210fffbe520934dc776945691b77
DolphinSh <1796520046@qq.com>
2022-06-14 14:36:14 +0800

    IASS基础服务的搭建和开发-网关基础设施的搭建

---
+++ coin-exchange/coin-iaas/gateway-service/src/main/java/com/dolphin/GatewayServerApplication.java
@@ -0,0 +1,83 @@
+package com.dolphin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient //服务发现
+public class GatewayServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class,args);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
