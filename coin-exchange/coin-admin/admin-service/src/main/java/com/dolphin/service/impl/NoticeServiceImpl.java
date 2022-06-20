52c26004616e13c13476d440676b433c9523fc7e
DolphinSh <1796520046@qq.com>
2022-06-20 17:42:23 +0800

    服务中台_后台管理系统的开发 - 公告管理 查询 | 删除 | 启禁用
    添加因为涉及到文件的上传，因此需要先完成添加后在做公告添加功能

--- coin-exchange/coin-admin/admin-service/src/main/java/com/dolphin/service/impl/NoticeServiceImpl.java
+++ coin-exchange/coin-admin/admin-service/src/main/java/com/dolphin/service/impl/NoticeServiceImpl.java
@@ -1,5 +1,8 @@
 package com.dolphin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
@@ -10,4 +13,22 @@ import com.dolphin.service.NoticeService;
 @Service
 public class NoticeServiceImpl extends ServiceImpl<NoticeMapper, Notice> implements NoticeService{
 
+    /**
+     * 条件分页查询
+     *
+     * @param page      分页参数
+     * @param title     公告的标签
+     * @param startTime 公告创建的开始时间
+     * @param endTime   公告创建的结束时间
+     * @param status    公告当前的状态
+     * @return
+     */
+    @Override
+    public Page<Notice> findByPage(Page<Notice> page, String title, String startTime, String endTime, Integer status) {
+        return page(page,new LambdaQueryWrapper<Notice>()
+                .like(!StringUtils.isEmpty(title),Notice::getTitle,title)
+                .between(!StringUtils.isEmpty(startTime)&&!StringUtils.isEmpty(endTime),Notice::getCreated,startTime,endTime+" 23:59:59")
+                .eq(status!=null,Notice::getStatus,status)
+        );
+    }
 }
