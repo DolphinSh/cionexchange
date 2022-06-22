7b353814bb6da0a1d48f3c8af741d0c53d1c9b21
DolphinSh <1796520046@qq.com>
2022-06-22 16:11:24 +0800

    服务中台_会员系统的开发-访问测试

--- coin-exchange/coin-member/member-service/src/main/java/com/dolphin/MemberServiceApplication.java
+++ coin-exchange/coin-member/member-service/src/main/java/com/dolphin/MemberServiceApplication.java
@@ -3,9 +3,11 @@ package com.dolphin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient //开启服务发现
+@EnableFeignClients
 public class MemberServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemberServiceApplication.class,args);
