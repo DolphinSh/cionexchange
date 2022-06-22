5470d936e9a8e985e1c633d212f186f937fa6e3b
DolphinSh <1796520046@qq.com>
2022-06-22 15:59:01 +0800

    服务中台_后台管理系统的开发-参数配置管理

--- coin-exchange/coin-admin/admin-service/src/main/java/com/dolphin/service/ConfigService.java
+++ coin-exchange/coin-admin/admin-service/src/main/java/com/dolphin/service/ConfigService.java
@@ -1,8 +1,17 @@
 package com.dolphin.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dolphin.domain.Config;
 import com.baomidou.mybatisplus.extension.service.IService;
 public interface ConfigService extends IService<Config>{
 
-
+    /**
+     * 条件分页查询后台参数
+     * @param page
+     * @param type
+     * @param name
+     * @param code
+     * @return
+     */
+    Page<Config> findByPage(Page<Config> page, String type, String name, String code);
 }
--- coin-exchange/coin-admin/admin-service/src/main/java/com/dolphin/service/impl/ConfigServiceImpl.java
+++ coin-exchange/coin-admin/admin-service/src/main/java/com/dolphin/service/impl/ConfigServiceImpl.java
@@ -1,13 +1,37 @@
 package com.dolphin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
 import javax.annotation.Resource;
 import java.util.List;
+
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dolphin.mapper.ConfigMapper;
 import com.dolphin.domain.Config;
 import com.dolphin.service.ConfigService;
-@Service
-public class ConfigServiceImpl extends ServiceImpl<ConfigMapper, Config> implements ConfigService{
 
+@Service
+public class ConfigServiceImpl extends ServiceImpl<ConfigMapper, Config> implements ConfigService {
+
+    /**
+     * 条件分页查询后台参数
+     *
+     * @param page
+     * @param type
+     * @param name
+     * @param code
+     * @return
+     */
+    @Override
+    public Page<Config> findByPage(Page<Config> page, String type, String name, String code) {
+        Page<Config> pageData = page(page, new LambdaQueryWrapper<Config>()
+                .like(!StringUtils.isEmpty(type), Config::getType, type)
+                .like(!StringUtils.isEmpty(code), Config::getCode, code)
+                .like(!StringUtils.isEmpty(name), Config::getName, name)
+        );
+        return pageData;
+    }
 }
