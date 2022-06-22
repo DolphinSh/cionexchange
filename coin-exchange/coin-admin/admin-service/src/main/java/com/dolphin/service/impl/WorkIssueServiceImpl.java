786f7c62a47c4cac50400700f8558ab9230da8f2
DolphinSh <1796520046@qq.com>
2022-06-22 15:30:49 +0800

    服务中台_后台管理系统的开发-公司银行卡管理完成

--- coin-exchange/coin-admin/admin-service/src/main/java/com/dolphin/service/impl/WorkIssueServiceImpl.java
+++ coin-exchange/coin-admin/admin-service/src/main/java/com/dolphin/service/impl/WorkIssueServiceImpl.java
@@ -1,13 +1,37 @@
 package com.dolphin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
 import java.util.List;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dolphin.mapper.WorkIssueMapper;
 import com.dolphin.domain.WorkIssue;
 import com.dolphin.service.WorkIssueService;
+import org.springframework.util.CollectionUtils;
+
 @Service
 public class WorkIssueServiceImpl extends ServiceImpl<WorkIssueMapper, WorkIssue> implements WorkIssueService{
 
+    /**
+     * 条件分页查询工单列表
+     *
+     * @param page      分页参数
+     * @param status    工单的状态
+     * @param startTime 查询的工单创建起始时间
+     * @param endTime   查询的工单创建截至时间
+     * @return
+     */
+    @Override
+    public Page<WorkIssue> findByPage(Page<WorkIssue> page, Integer status, String startTime, String endTime) {
+        Page<WorkIssue> pageData = page(page, new LambdaQueryWrapper<WorkIssue>()
+                .eq(status != null, WorkIssue::getStatus, status)
+                .between(
+                        !StringUtils.isEmpty(startTime) && !StringUtils.isEmpty(endTime),
+                        WorkIssue::getCreated,
+                        startTime, endTime + " 23:59:59")
+        );
+        return pageData;
+    }
 }
