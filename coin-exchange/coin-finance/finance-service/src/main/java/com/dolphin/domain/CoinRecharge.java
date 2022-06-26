9cc4d633713a47a1905bbd7fb11e9ed35b81ca1a
DolphinSh <1796520046@qq.com>
2022-06-26 16:36:03 +0800

    服务中台_财务系统的开发-充币记录

--- coin-exchange/coin-finance/finance-service/src/main/java/com/dolphin/domain/CoinRecharge.java
+++ coin-exchange/coin-finance/finance-service/src/main/java/com/dolphin/domain/CoinRecharge.java
@@ -1,9 +1,6 @@
 package com.dolphin.domain;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.math.BigDecimal;
@@ -91,16 +88,24 @@ public class CoinRecharge {
     /**
      * 修改时间
      */
-    @TableField(value = "last_update_time")
+    @TableField(value = "last_update_time",fill = FieldFill.INSERT_UPDATE)
     @ApiModelProperty(value="修改时间")
     private Date lastUpdateTime;
 
     /**
      * 创建时间
      */
-    @TableField(value = "created")
+    @TableField(value = "created",fill = FieldFill.INSERT)
     @ApiModelProperty(value="创建时间")
     private Date created;
+
+    @TableField(exist = false)
+    @ApiModelProperty(value = "用户的名称")
+    private String username ;
+
+    @TableField(exist = false)
+    @ApiModelProperty(value = "用户的真实名称")
+    private String realName ;
 
     public static final String COL_ID = "id";
 
